adfffa7cfe7a8ca8758ecf2d0be2c39d6233d8ff
Dillz42 <dillz42@gmail.com>
2016-06-27 21:49:57 -0500

    Initial commit to Github

---
+++ BackendToolBox/Tools.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace BackendToolBox
+{
+    public class Tools
+    {
+        static void Main(string[] args)
+        {
+            Wallpaper.Set("C:\\Users\\Dillon\\Desktop\\Reddit Wallpapers\\test.jpg");
+        }
+
+        public static string httpRequest(string uri)
+        {
+            WebRequest request = WebRequest.Create(uri);
+            ((HttpWebRequest)request).UserAgent = ".NET Framework Example Client";
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+
+            string responseMessage = "";
+            char ch = (char)dataStream.ReadByte();
+            while ((byte)ch != 255)
+            {
+                responseMessage += ch;
+                ch = (char)dataStream.ReadByte();
+            }
+
+            return responseMessage;
+        }
+
+        public static void downloadFile(string url, string fileName)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(url, fileName);
+        }
+    }
+}
---
+++ BackendToolBox/Wallpaper.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BackendToolBox
+{
+    public sealed class Wallpaper
+    {
+        Wallpaper() { }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public static void Set(string path)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                path,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
