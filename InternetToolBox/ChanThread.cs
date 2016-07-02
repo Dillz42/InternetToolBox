d0fa940577807378800ed5470d4596da9cf57128
Dillz42 <dillz42@gmail.com>
2016-07-01 23:05:25 -0500

    Adding missed files

---
+++ InternetToolBox/ChanThread.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace InternetToolBox
+{
+    public class ChanThread
+    {
+        private Tools tools;
+
+        private const int SLEEP_TIME_DEFAULT = 20000;
+        private const int SLEEP_TIME_MAX = 300000;
+        private const int SLEEP_TIME_STEP = 10000;
+        private int sleepTime = SLEEP_TIME_DEFAULT; public int SleepTime { get { return sleepTime; } }
+
+        private int threadId;   public int ThreadId { get { return threadId; } }
+        private string board;   public string Board { get { return board; } }
+        private string path;    public string Path { get { return path; } }
+        
+        public ChanThread(int threadId, string board, Tools tools)
+        {
+            this.tools = tools;
+            this.threadId = threadId;
+            this.board = board;
+
+            JObject jsonObject = null;
+            try
+            {
+                jsonObject = JObject.Parse(tools.httpRequest(
+                        "http://a.4cdn.org/" + board + "/thread/" + threadId + ".json"));
+            }
+            catch (Exception e)
+            {
+                tools.log("Error: " + e.Message);
+                tools.log("Error: " + e.StackTrace);
+                return;
+            }
+
+            path = "D:\\Downloads\\PicsAndVids\\FromChan\\" + board + "-" + threadId + "-" + jsonObject["posts"][0]["semantic_url"] + "\\";
+            if(Directory.Exists(path) == false)
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        public void run()
+        {
+            while (true)
+            {
+                try
+                {
+                    JObject jsonObject = JObject.Parse(tools.httpRequest("http://a.4cdn.org/" + board + "/thread/" + threadId + ".json"));
+                    File.WriteAllText(path + "thread.json", jsonObject.ToString());
+
+                    List<Downloader> downloaderList = new List<Downloader>();
+                    for (int i = 0; i < ((JArray)jsonObject["posts"]).Count; i++)
+                    {
+                        if (jsonObject["posts"][i]["tim"] != null)
+                        {
+                            Downloader downloader = new Downloader(
+                                "http://i.4cdn.org/" + board + "/" +
+                                    jsonObject["posts"][i]["tim"] +
+                                    jsonObject["posts"][i]["ext"],
+                                path,
+                                jsonObject["posts"][i]["tim"] + "-" +
+                                    jsonObject["posts"][i]["filename"] +
+                                    jsonObject["posts"][i]["ext"],
+                                true, tools);
+                            downloaderList.Add(downloader);
+                        }
+                    }
+
+                    if (downloaderList.Count == 0)
+                    {
+                        switch (board)
+                        {
+                            case "b":
+                                sleepTime += SLEEP_TIME_STEP/2;
+                                if (sleepTime > SLEEP_TIME_MAX/5)
+                                    sleepTime = SLEEP_TIME_MAX/5;
+                                break;
+                            default:
+                                sleepTime += SLEEP_TIME_STEP;
+                                if (sleepTime > SLEEP_TIME_MAX)
+                                    sleepTime = SLEEP_TIME_MAX;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        sleepTime = SLEEP_TIME_DEFAULT;
+                        foreach (Downloader downloader in downloaderList)
+                        {
+                            if (File.Exists(downloader.LocalDir + downloader.FileName) == false)
+                            {
+                                downloader.start();
+                            }
+                        }
+                        foreach (Downloader downloader in downloaderList)
+                        {
+                            downloader.join();
+                        }
+                    }
+                    tools.log("Thread " + board + "-" + threadId + " updated");
+                    Thread.Sleep(sleepTime);
+                }
+                catch (Exception e)
+                {
+                    tools.log("Error: " + e.Message);
+                    tools.log("Error: " + e.StackTrace);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ InternetToolBox/Downloader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace InternetToolBox
+{
+    class Downloader 
+    {
+
+        private string url;         public string Url{get{return url;}}
+        private string localDir;    public string LocalDir { get { return localDir; } }
+        private string fileName;    public string FileName { get { return fileName; } }
+
+        private ThreadStart threadStart;
+        private Thread thread;
+        private Tools tools;
+
+        public Downloader(string url, string localDir, string fileName, bool waitToDownload, Tools tools)
+        {
+            this.tools = tools;
+            this.url = url;
+            this.localDir = localDir;
+            this.fileName = fileName;
+            
+            threadStart = new ThreadStart(startDownload);
+            thread = new Thread(threadStart);
+            if (waitToDownload == false)
+                thread.Start();
+        }
+
+        public void start()
+        {
+            thread.Start();
+        }
+
+        private void startDownload()
+        {
+            tools.downloadFile(url, localDir + fileName);
+        }
+
+        public bool isAlive()
+        {
+            return thread.IsAlive;
+        }
+
+        public void join()
+        {
+            if(thread.IsAlive)
+                thread.Join();
+        }
+    }
+}
